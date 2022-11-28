d8d8269724c6519b40698cbe276fa784efc9af0b
dermasugita <itsukiisogin@gmail.com>
2022-11-28 16:11:18 +0000

    add new file

--- src/vcf2bedpe.rs
+++ src/vcf2bedpe.rs
@@ -1,10 +1,23 @@
 use crate::Vcf2bedpe;
 
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+
 pub fn main(vcf2bedpe: &Vcf2bedpe) {
-    println!("hello");
+    let filepath = &vcf2bedpe.vcf[0];
+    let vcf_file = File::open(filepath);
+    let mut reader = BufReader::new(vcf_file.unwrap());
+    for line in reader.lines() {
+        println!("{}", line.unwrap());
+    }
+    
 }
 
-#[test]
-fn it_works() {
-    assert_eq!(2+2, 4);
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2+2, 4);
+    }
 }
