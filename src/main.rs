9006fd3804903d6a51c6d59d53164a330d8cd39f
dermasugita <itsukiisogin@gmail.com>
2022-11-30 14:17:27 +0000

    added: intersect.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod vcf2bedpe;
+mod intersect;
 use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser)]
@@ -12,11 +13,18 @@ struct Cli {
 enum Commands {
     /// Convert VCF files into BEDPE files.
     Vcf2bedpe(Vcf2bedpe),
+    Intersect(Intersect),
 }
 
 #[derive(Args)]
 pub struct Vcf2bedpe {
     vcf: Vec<std::path::PathBuf>
+}
+
+#[derive(Args)]
+pub struct Intersect {
+    file1: Vec<std::path::PathBuf>,
+    file2: Vec<std::path::PathBuf>,
 }
 
 pub fn main() {
@@ -25,6 +33,9 @@ pub fn main() {
     match &cli.command {
         Commands::Vcf2bedpe(vcf2bedpe) => {
             vcf2bedpe::main(vcf2bedpe);
+        }
+        Commands::Intersect(intersect) => {
+            intersect::main(intersect);
         }
     }
 }
